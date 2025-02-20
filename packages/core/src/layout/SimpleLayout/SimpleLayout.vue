3b748ffa57a465da61b8bcbe14545906551ce082
Ronan <crnoogle@gmail.com>
2025-02-20 16:44:22 +0800

    feat: 优化SimpleLayout dark主题下bg颜色

--- packages/core/src/layout/SimpleLayout/SimpleLayout.vue
+++ packages/core/src/layout/SimpleLayout/SimpleLayout.vue
@@ -148,7 +148,7 @@ onMounted(() => {
         </template>
 
         <!-- content -->
-        <div class="flex-1 bg-primary-50/30 dark:bg-black">
+        <div class="flex-1 bg-primary-50/30 dark:bg-neutral-900">
           <slot />
           <NBackTop />
         </div>
